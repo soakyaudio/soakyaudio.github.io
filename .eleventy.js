d03e39f0b29365417dd371bc51694666aef49a7b
Micha Hanselmann <micha.hanselmann@gmail.com>
2022-12-29 20:57:55 +0100

    initial page (#1)
    * stash
    
    * asd
    
    * asdw
    
    * as
    
    * asd
    
    * asd
    
    * asd
    
    * all in
    
    * yup

---
+++ .eleventy.js
@@ -0,0 +1,37 @@
+const { DateTime } = require("luxon");
+const markdownIt = require("markdown-it");
+const markdownItAnchor = require("markdown-it-anchor")
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight")
+
+module.exports = (eleventyConfig) => {
+    // plugins
+    eleventyConfig.addPlugin(syntaxHighlight);
+
+    // passthrough
+    eleventyConfig.addPassthroughCopy("css");
+    eleventyConfig.addPassthroughCopy("img");
+
+    // date formatters
+    eleventyConfig.addFilter("readableDate", (date) => {
+        return DateTime.fromJSDate(date, { zone: "utc" }).toLocaleString(DateTime.DATE_FULL);
+    });
+    eleventyConfig.addFilter('htmlDateString', (date) => {
+        return DateTime.fromJSDate(date, { zone: "utc" }).toISODate();
+    });
+
+    // year shortcode
+    eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
+
+    // markdown rendering: anchors for all headings
+    const markdownLibrary = markdownIt({
+        html: true,
+    }).use(markdownItAnchor, {
+        permalink: markdownItAnchor.permalink.ariaHidden({
+            class: "anchor-link",
+            placement: "after",
+            symbol: "#",
+        }),
+        level: [1, 2, 3],
+    });
+    eleventyConfig.setLibrary("md", markdownLibrary);
+};
---
+++ designer.md
@@ -0,0 +1,19 @@
+---
+layout: layouts/page.njk
+title: soaky designer
+---
+## Portable sound design studio
+soaky designer lets you create audio experiences the world hasn't heard before! Be it soaring synths, heavenly pads or experimental effects, designer's portable editor comes with everything you need for modern sound design. Mix and combine DSP operations in real-time to explore fundamentals of sound & synthesis, scribble musical ideas or add something novel to your next live performance.
+
+![soaky designer screenshot](/img/soakydesigner.png)
+
+## Key features
+- 25+ modular blocks for waveform generation, filtering, math, MIDI, ...
+- Intuitive gesture-based graph editor with real-time playback
+- Connect your hardware with out of the box MIDI support
+- Enjoy the same features across iPhone, iPad & Mac
+
+Last but not least, I'd love to [hear from you](mailto:hello@soakyaudio.com)! Tell me your story, how you're using soaky designer, what sounds you've created and what features you're missing. I'm as learning and excited about this audio stuff as you are :).
+
+## Privacy policy
+soaky designer does not collect, transmit, distribute or sell your data.
