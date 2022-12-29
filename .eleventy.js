4b8d803501cc7f9687a5e003a3038ac9c6bac794
Micha Hanselmann <micha.hanselmann@gmail.com>
2022-12-29 17:52:41 +0100

    asd

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,10 @@
 const { DateTime } = require("luxon");
 
 module.exports = (eleventyConfig) => {
+    // passthrough
+    eleventyConfig.addPassthroughCopy("css");
+    eleventyConfig.addPassthroughCopy("img");
+
     // date formatters
     eleventyConfig.addFilter("readableDate", (date) => {
         return DateTime.fromJSDate(date, { zone: "utc" }).toLocaleString(DateTime.DATE_FULL);
@@ -8,4 +12,7 @@ module.exports = (eleventyConfig) => {
     eleventyConfig.addFilter('htmlDateString', (date) => {
         return DateTime.fromJSDate(date, { zone: "utc" }).toISODate();
     });
+
+    // year shortcode
+    eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
 };
